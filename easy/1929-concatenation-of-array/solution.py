bdb1a420cbbb42c34de0e03e3f1e4c32efbe4303
davidsheridan3 <davidsheridan061@gmail.com>
2026-09-16 13:20:59 +0100

    1929. Concatenation of Array - simplified solution

--- easy/1929-concatenation-of-array/solution.py
+++ easy/1929-concatenation-of-array/solution.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def getConcatenation(self, nums):
         ans = []
-        i = 0
 
-        while i < 2:
+        for i in range(2):
             for num in nums:
                 ans.append(num)
-            i += 1
 
         return ans
