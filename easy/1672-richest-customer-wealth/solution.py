24435cf5d036c4cbfab5600ce11fad70f2ad3e60
davidsheridan3 <davidsheridan061@gmail.com>
2026-09-18 11:53:47 +0100

    1480. Richest Customer Wealth - slight refinement

--- easy/1672-richest-customer-wealth/solution.py
+++ easy/1672-richest-customer-wealth/solution.py
@@ -4,6 +4,6 @@ class Solution(object):
         for account in accounts:
             account_total = sum(account)
             if account_total > richest_person:
-                richest_person = 0 + account_total
+                richest_person = account_total
         return richest_person
 
